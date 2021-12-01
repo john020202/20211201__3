d59e3f966ab6e9b27899f2a09cf48369d3cd9ac4
송승준 <john091122@naver.com>
2021-12-01 09:24:44 +0900

    1. 첫 프로젝트 실습 시작
    2. 기본 Person 객체 생성과 People객체 생성 및
    추가 버튼 UI 작성
    
    3. Person 클래스와 People 클래스를 파일로 모듈화하고 추가버튼 클릭 시 텍스트박스의 내용을 비우고 포커스를 지정함.
    
    4. 검색, 삭제 버튼 UI 작성

---
+++ telphoneapp_cs21-master/TelPhoneApp/MainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TelPhoneApp {
+    public partial class MainForm : Form {
+        People pList = new People();
+        public MainForm() {
+            InitializeComponent();
+        }
+        private void btnAdd_Click(object sender, EventArgs e) {
+            if (txtName.Text != "" && txtPhone.Text != "") {
+                Person per = new Person(txtName.Text, txtPhone.Text);
+                pList.Add(per);
+
+                txtName.Text = "";
+                txtPhone.Text = "";
+                txtName.Focus();
+                UpdateDisplay();
+            }
+        }
+        private void UpdateDisplay() {
+            lbDisplay.Items.Clear();
+            for (int i = 0; i < pList.Count; ++i)
+                lbDisplay.Items.Add(pList[i].ToString());
+        }
+    }
+}
---
+++ telphoneapp_cs21-master/TelPhoneApp/People.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TelPhoneApp {
+    public class People {
+        List<Person> perList = new List<Person>();
+        public void Add(Person per) {
+            perList.Add(per);
+        }
+        public void AddRange(Person[] parr) {
+            perList.AddRange(parr);
+        }
+        public Person this[int idx] {
+            get { return perList[idx]; }
+        }
+        public int Count {
+            get { return perList.Count; }
+        }
+    }
+}
